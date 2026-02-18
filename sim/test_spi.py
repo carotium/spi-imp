8fe8d2c74e7cfa09a8a06cfa892d23d944fe99ef
carotium <coolboy9987@gmail.com>
2026-02-18 12:39:13 +0000

    Added scheduled obi channel a write transaction

--- sim/test_spi.py
+++ sim/test_spi.py
@@ -3,28 +3,20 @@ from forastero.driver import DriverEvent
 from forastero import BaseBench
 
 from base import get_test_runner, WAVES
-from handshake.io import HandshakeIO
-from handshake.requestor import HandshakeRequestMonitor, HandshakeRequestDriver
-from handshake.responder import HandshakeResponderDriver
-from handshake.sequences import handshake_ready_seq, handshake_rand_data_seq
-from handshake.transaction import HandshakeValid
+from handshake.io import ObiChAIO, ObiChRIO
+from handshake.requestor import ObiChARequestDriver
+from handshake.sequences import obi_channel_a_write_trans
 
 class SpiImpTB(BaseBench):
     def __init__(self, dut):
         super().__init__(dut, clk=dut.clk_i, rst=dut.rstn_i, rst_active_high=False)
-        input_io = HandshakeIO(dut, "input", IORole.RESPONDER, io_style=io_suffix_style)
-        output_io = HandshakeIO(dut, "output", IORole.INITIATOR, io_style=io_suffix_style)
-        self.register("input_driver", HandshakeRequestDriver(self, input_io, self.clk, self.rst))
-        self.register("output_driver", HandshakeResponderDriver(self, output_io, self.clk, self.rst, blocking=False))
-        self.register("output_monitor", HandshakeRequestMonitor(self, output_io, self.clk, self.rst))
+        obi_a_io = ObiChAIO(dut, "obi", IORole.RESPONDER, io_style=io_suffix_style)
+        obi_r_io = ObiChRIO(dut, "obi", IORole.RESPONDER, io_style=io_suffix_style)
+
+        self.register("obi_a_drv", ObiChARequestDriver(self, obi_a_io, self.clk, self.rst))
 
         # Register callback on input driver to push reference to monitor
-        self.input_driver.subscribe(DriverEvent.ENQUEUE, self.push_reference)
-
-    def push_reference(self, driver: HandshakeRequestDriver, event: DriverEvent, obj: HandshakeValid) -> None:
-        assert driver is self.input_driver
-        assert event == DriverEvent.ENQUEUE
-        self.scoreboard.channels["output_monitor"].push_reference(obj)
+        #self.input_driver.subscribe(DriverEvent.ENQUEUE, self.push_reference)
 
 @SpiImpTB.testcase(
     reset_wait_during=2,
@@ -35,9 +27,9 @@ class SpiImpTB(BaseBench):
 )
 async def random_raffic(tb: SpiImpTB, log):
     log.info(f"Scheduling random traffic to the SPI input.")
-    tb.schedule(handshake_rand_data_seq(input_drv=tb.input_driver, length=100, delay_range=(0, 5)))
-    log.info(f"Scheduling random ready backpressure.")
-    tb.schedule(handshake_ready_seq(ready_drv=tb.output_driver), blocking=False)
+    tb.schedule(obi_channel_a_write_trans(obi_a_drv=tb.obi_a_drv, address=0x0010, data=123))
+    tb.schedule(obi_channel_a_write_trans(obi_a_drv=tb.obi_a_drv, address=0x0100, data=456))
+
 
 def test_spi_runner():
     runner = get_test_runner("spi_imp")
