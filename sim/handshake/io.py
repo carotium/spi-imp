e01b46da8e7734bbd34ab86832aabb3a2b90a9ae
carotium <coolboy9987@gmail.com>
2026-02-18 12:36:47 +0000

    Added seperate channel A and R IOs

--- sim/handshake/io.py
+++ sim/handshake/io.py
@@ -7,7 +7,7 @@ from cocotb.handle import HierarchyObject
 from forastero.io import BaseIO, IORole
 
 
-class HandshakeIO(BaseIO):
+class ObiChAIO(BaseIO):
     def __init__(
         self,
         dut: HierarchyObject,
@@ -15,4 +15,28 @@ class HandshakeIO(BaseIO):
         role: IORole,
         io_style: Callable[[str | None, str, IORole, IORole], str] | None = None,
     ):
-        super().__init__(dut, name, role, ["data", "valid"], ["ready"], io_style)
+        super().__init__(
+            dut, 
+            name, 
+            role, 
+            ["req", "addr", "wdata", "we", "be"], 
+            ["gnt"], 
+            io_style
+        )
+
+class ObiChRIO(BaseIO):
+    def __init__(
+        self,
+        dut: HierarchyObject,
+        name: str,
+        role: IORole,
+        io_style: Callable[[str | None, str, IORole, IORole], str] | None = None,
+    ):
+        super().__init__(
+            dut, 
+            name, 
+            role, 
+            [], 
+            ["rvalid", "rdata"], 
+            io_style
+        )
