013ebc755c3d55a244a43dc46cfb511134d5bc1c
carotium <coolboy9987@gmail.com>
2026-02-18 12:37:56 +0000

    Added obi channel a write transaction

--- sim/handshake/sequences.py
+++ sim/handshake/sequences.py
@@ -8,51 +8,18 @@ from forastero.driver import DriverEvent
 from forastero.monitor import MonitorEvent
 from forastero.sequence import SeqContext, SeqProxy
 
-from .requestor import HandshakeRequestMonitor, HandshakeRequestDriver
-from .responder import HandshakeResponderDriver
-from .transaction import HandshakeReady, HandshakeValid
+from .transaction import ObiChATrans
+from .requestor import ObiChARequestDriver
 
 @forastero.sequence(auto_lock=True)
-@forastero.requires("ready_drv", HandshakeResponderDriver)
-async def handshake_ready_seq(
+@forastero.requires("obi_a_drv", ObiChARequestDriver)
+async def obi_channel_a_write_trans(
     ctx: SeqContext,
-    ready_drv: SeqProxy[HandshakeResponderDriver],
-    delay_range: tuple[int, int] = (1, 1),
+    obi_a_drv: SeqProxy[ObiChARequestDriver],
+    address: int,
+    data: int,
 ) -> None:
-    min_delay, max_delay = min(delay_range), max(delay_range)
-    while True:
-        await ready_drv.enqueue(
-            HandshakeReady(ready=True, delay=ctx.random.randint(min_delay, max_delay)),
-            wait_for=DriverEvent.POST_DRIVE,
-        ).wait()
-
-@forastero.sequence(auto_lock=True)
-@forastero.requires("ready_drv", HandshakeResponderDriver)
-async def handshake_independent_ready_seq(
-    ctx: SeqContext,
-    ready_drv: SeqProxy[HandshakeResponderDriver],
-    delay_range: tuple[int, int] = (1, 1),
-) -> None:
-    min_delay, max_delay = min(delay_range), max(delay_range)
-    while True:
-        await ready_drv.enqueue(
-            HandshakeReady(ready=True, delay=ctx.random.randint(min_delay, max_delay)),
-            wait_for=DriverEvent.POST_DRIVE,
-        ).wait()
-
-@forastero.sequence(auto_lock=True)
-@forastero.requires("input_drv", HandshakeRequestDriver)
-async def handshake_rand_data_seq(
-    ctx: SeqContext,
-    input_drv: SeqProxy[HandshakeRequestDriver],
-    length: int = 1,
-    delay_range: tuple[int, int] = (0, 0)
-) -> None:
-    min_delay, max_delay = min(delay_range), max(delay_range)
-    for _ in range(length):
-        delay = ctx.random.randint(min_delay, max_delay)
-        await ClockCycles(ctx.clk, delay)
-        await input_drv.enqueue(
-            HandshakeValid(data=ctx.random.randint(0, (2**32)-1), valid=True),
-            wait_for=DriverEvent.POST_DRIVE,
-        ).wait()
+    await obi_a_drv.enqueue(
+        ObiChATrans(addr=address, wdata=data, we=True, be=0b1111),
+        wait_for=DriverEvent.POST_DRIVE,
+    ).wait()
