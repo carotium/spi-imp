4e32841e54a933a5cafa4896c5b87279aa8196be
carotium <coolboy9987@gmail.com>
2026-02-18 14:12:43 +0000

    Deleted unused code

--- sim/handshake/responder.py
+++ sim/handshake/responder.py
@@ -3,20 +3,3 @@
 
 from cocotb.triggers import ClockCycles, RisingEdge
 from forastero.driver import BaseDriver
-
-from .transaction import HandshakeReady
-
-class HandshakeResponderDriver(BaseDriver):
-    async def drive(self, transaction: HandshakeReady):
-        # Wait for a request to be presented
-        while self.io.get("valid") == 0:
-            await RisingEdge(self.clk)
-        # Wait for the required number of cycles
-        if transaction.delay > 0:
-            await ClockCycles(self.clk, transaction.delay)
-        # Drive the acknowledgement
-        self.io.set("ready", transaction.ready)
-        # Wait a cycle
-        await RisingEdge(self.clk)
-        # Clear the ready
-        self.io.set("ready", 0)
