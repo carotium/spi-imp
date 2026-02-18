309de29491c8aad0244689b2855b9364bb106f6b
carotium <coolboy9987@gmail.com>
2026-02-18 12:37:16 +0000

    Added obi channel a request driver

--- sim/handshake/requestor.py
+++ sim/handshake/requestor.py
@@ -5,26 +5,17 @@ from cocotb.triggers import RisingEdge
 from forastero.driver import BaseDriver
 from forastero.monitor import BaseMonitor
 
-from .transaction import HandshakeValid
+from .transaction import ObiChATrans
 
-class HandshakeRequestDriver(BaseDriver):
-    async def drive(self, transaction: HandshakeValid):
-        # Setup the transaction
-        self.io.set("data", transaction.data)
-        self.io.set("valid", transaction.valid)
-        # Wait one cycle for setup
+class ObiChARequestDriver(BaseDriver):
+    async def drive(self, transaction: ObiChATrans):
+        self.io.set("addr", transaction.addr)
+        self.io.set("wdata", transaction.wdata)
+        self.io.set("we", transaction.we)
+        self.io.set("be", transaction.be)
+
+        self.io.set("req", 1)
         await RisingEdge(self.clk)
-        # Wait for the acknowledgement
-        while self.io.get("ready") == 0:
+        while self.io.get("gnt") == 0:
             await RisingEdge(self.clk)
-        # Clear the request
-        self.io.set("valid", 0)
-
-class HandshakeRequestMonitor(BaseMonitor):
-    async def monitor(self, capture):
-        while True:
-            await RisingEdge(self.clk)
-            if self.rst.value == 0:
-                continue
-            if self.io.get("valid") and self.io.get("ready"):
-                capture(HandshakeValid(data=self.io.get("data"), valid=True))
+        self.io.set("req", 0)
