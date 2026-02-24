e2fa3f8d3417007b7a27e1c466d6990878149817
carotium <coolboy9987@gmail.com>
2026-02-24 15:14:44 +0000

    Fixed requestor so it extracts bit by bit of SPI transfer and captures it.

--- sim/handshake/requestor.py
+++ sim/handshake/requestor.py
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2023-2024 Vypercore. All Rights Reserved
 
-from cocotb.triggers import RisingEdge
+from cocotb.triggers import RisingEdge, FallingEdge
 from forastero.driver import BaseDriver
 from forastero.monitor import BaseMonitor
 
-from .transaction import ObiChATrans, ObiChRTrans, SpiTransferTrans
+from .transaction import ObiChATrans, ObiChRTrans, SpiTrans
 
 class ObiChARequestDriver(BaseDriver):
     async def drive(self, transaction: ObiChATrans):
@@ -24,16 +24,19 @@ class ObiChARequestDriver(BaseDriver):
         await RisingEdge(self.clk)
 
 class SpiRequestDriver(BaseDriver):
-    async def drive(self, transaction: SpiTransferTrans):
-        self.io.set("ss", transaction.ss)
-        self.io.set("sclk", transaction.sclk)
-        self.io.set("mosi", transaction.mosi)
-        #self.io.set("miso", transaction.miso)
+    async def drive(self, transaction: SpiTrans):
+        binary_str = format(transaction.data, '>08b')
 
-        self.io.set("miso", True)
         while self.io.get("ss") == True:
-            await RisingEdge(self.clk)
-        await RisingEdge(self.clk)
+            await FallingEdge(self.clk)
+        for bit in binary_str:
+            assert self.io.get("ss") == 0, "Slave select must not be deasserted during operation!"
+            while self.io.get("sclk") != 1:
+                await RisingEdge(self.clk)
+            self.io.set("mosi", int(bit))
+            while self.io.get("sclk") != 0:
+                await RisingEdge(self.clk)
+        self.io.set("mosi", 0)
 
 
 class ObiChRRequestMonitor(BaseMonitor):
@@ -48,3 +51,37 @@ class ObiChRRequestMonitor(BaseMonitor):
                         rdata = self.io.get("rdata"),
                     )
                 )
+
+class SpiMonitor(BaseMonitor):
+    async def monitor(self, capture):
+        index = 7
+        spi_data = 0
+        captured = 0
+        while True:
+            # wait for reset to deassert
+            await RisingEdge(self.clk)
+            if(self.rst.value == 0):
+                index = 7
+                spi_data = 0
+                captured = 0
+                await RisingEdge(self.clk)
+
+            while(self.io.get("sclk") == True):
+                await RisingEdge(self.clk)
+
+            while(self.io.get("ss") == False and index >= 0):
+                if(self.io.get("sclk") == True and captured == 0):
+                    print(f"{index})mosi={int(self.io.get("mosi"))}")
+                    spi_data += (int(self.io.get("mosi")) * 2 ** index)
+                    captured = 1
+                    index -= 1
+                elif(self.io.get("sclk") == False):
+                    captured = 0
+                
+                await RisingEdge(self.clk)
+
+            capture(
+                SpiTrans(
+                    data = spi_data
+                )
+            )
