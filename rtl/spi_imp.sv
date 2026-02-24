bc037a0ca7098f4c38223fec352170ffe8e7c470
carotium <coolboy9987@gmail.com>
2026-02-24 08:34:23 +0000

    Built write and read obi transaction for data register.

--- rtl/spi_imp.sv
+++ rtl/spi_imp.sv
@@ -30,138 +30,143 @@ module spi_imp #(
 
   output  logic                     spi_done_o
 );
-    localparam DataRegAddr = 0;
-    localparam CtrlRegAddr = 1;
+  localparam DataRegAddr = 0;
+  localparam CtrlRegAddr = 1;
 
-    logic [11:0] spi_sclk_counter;
+  logic [11:0] spi_sclk_counter;
 
-    logic [7:0] data_reg;
+  logic [7:0] data_reg;
 
-    logic ctrl_start_bit;    // 0 bit
-    logic ctrl_busy_bit;     // 1 bit
-    logic ctrl_complete_bit; // 2 bit
+  logic ctrl_start_bit;    // 0 bit
+  logic ctrl_busy_bit;     // 1 bit
+  logic ctrl_complete_bit; // 2 bit
 
-    logic obi_a_fire;
-    assign obi_a_fire = obi_req_i && obi_gnt_o;
+  logic obi_a_fire;
+  assign obi_a_fire = obi_req_i && obi_gnt_o;
 
-    logic spi_started_sending = 1'b0;
-    logic spi_stopped_sending = 1'b0;
+  logic spi_started_sending;
+  logic spi_stopped_sending;
 
 
-    // OBI
-    // Grant
-    always_ff @(posedge clk_i) begin
-      if (~rstn_i)
-        obi_gnt_o <= 1'b0;
-      else if (obi_req_i)
-        obi_gnt_o <= 1'b1;
-      else
-        obi_gnt_o <= 1'b0;
+  // OBI
+  // Grant
+  // Maybe need to add support if we want multiple transfers at same time
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      obi_gnt_o <= 1'b0;
+    else if (obi_req_i)
+      obi_gnt_o <= 1'b1;
+    else
+      obi_gnt_o <= 1'b0;
+  end
+
+  // Data register
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      data_reg <= '0;
+    else if (obi_we_i && obi_addr_i == DataRegAddr && obi_be_i[0])
+      data_reg <= obi_wdata_i[7:0];
     end
 
-    // Data register
-    always_ff @(posedge clk_i) begin
-        if (~rstn_i)
-            data_reg <= '0;
-        else if (obi_a_fire && obi_we_i && obi_addr_i == DataRegAddr && obi_be_i[0])
-            data_reg <= obi_wdata_i[7:0];
-    end
+  // Data output
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      obi_rdata_o <= '0;
+    else if (~obi_we_i && obi_addr_i == DataRegAddr && obi_be_i[0])
+      obi_rdata_o <= {24'b0, data_reg};
+    else if (~obi_we_i && obi_addr_i == CtrlRegAddr && obi_be_i[0])
+      obi_rdata_o <= {29'b0, ctrl_complete_bit, ctrl_busy_bit, ctrl_start_bit};
+  end
 
-    // Data output
-    always_ff @(posedge clk_i) begin
-        if (~rstn_i)
-            obi_rdata_o <= '0;
-        else if (obi_a_fire && ~obi_we_i && obi_addr_i == DataRegAddr && obi_be_i[0])
-            obi_rdata_o <= {24'b0, data_reg};
-        else if (obi_a_fire && ~obi_we_i && obi_addr_i == CtrlRegAddr && obi_be_i[0])
-            obi_rdata_o <= {29'b0, ctrl_complete_bit, ctrl_busy_bit, ctrl_start_bit};
-    end
+  // Valid output
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      obi_rvalid_o <= 1'b0;
+    else
+      obi_rvalid_o <= obi_a_fire;
+  end
 
-    // Valid output
-    always_ff @(posedge clk_i) begin
-        if (~rstn_i)
-            obi_rvalid_o <= 1'b0;
-        else
-            obi_rvalid_o <= obi_a_fire;
-    end
+  // CONTROL LOGIC
+  // Control start bit
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      ctrl_start_bit <= 1'b0;
+    else if (obi_a_fire && obi_we_i && obi_addr_i == CtrlRegAddr && obi_be_i[0] && ~ctrl_busy_bit)
+      ctrl_start_bit <= obi_wdata_i[0];
+    else if (spi_started_sending)
+      ctrl_start_bit <= 1'b0;
+  end
 
-    // CONTROL LOGIC
-    // Control start bit
-    always_ff @(posedge clk_i) begin
-        if (~rstn_i)
-            ctrl_start_bit <= 1'b0;
-        else if (obi_a_fire && obi_we_i && obi_addr_i == CtrlRegAddr && obi_be_i[0] && ~ctrl_busy_bit)
-            ctrl_start_bit <= obi_wdata_i[0];
-        else if (spi_started_sending)
-            ctrl_start_bit <= 1'b0;
-    end
+  // Control busy bit
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      ctrl_busy_bit <= 1'b0;
+    else if (spi_started_sending)
+      ctrl_busy_bit <= 1'b1;
+    else if (spi_stopped_sending)
+      ctrl_busy_bit <= 1'b0;
+  end
 
-    // Control busy bit
-    always_ff @(posedge clk_i) begin
-        if (~rstn_i)
-            ctrl_busy_bit <= 1'b0;
-        else if (spi_started_sending)
-            ctrl_busy_bit <= 1'b1;
-        else if (spi_stopped_sending)
-            ctrl_busy_bit <= 1'b0;
-    end
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      ctrl_complete_bit <= 1'b0;
+    else if (spi_stopped_sending)
+      ctrl_complete_bit <= 1'b1;
+    else if (obi_a_fire && obi_we_i && obi_addr_i == CtrlRegAddr && obi_be_i[0])
+      ctrl_complete_bit <= obi_wdata_i[2];
+  end
 
-    always_ff @(posedge clk_i) begin
-        if (~rstn_i)
-            ctrl_complete_bit <= 1'b0;
-        else if (spi_stopped_sending)
-            ctrl_complete_bit <= 1'b1;
-        else if (obi_a_fire && obi_we_i && obi_addr_i == CtrlRegAddr && obi_be_i[0])
-            ctrl_complete_bit <= obi_wdata_i[2];
-    end
+  // SPI
+  // change to state machine
+    
+  // three states: sending, done_sending, idle
+    
+  //   SPI start sending
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      spi_started_sending <= 1'b0;
+    else if (ctrl_start_bit)
+      spi_started_sending <= 1'b1;
+    else
+      spi_started_sending <= 1'b0;
+  end
 
-    // SPI
-    //   SPI start sending
-    always_ff @(posedge clk_i) begin
-      if (~rstn_i)
-        spi_started_sending <= 1'b0;
-      else if (ctrl_start_bit)
-        spi_started_sending <= 1'b1;
-      else
-        spi_started_sending <= 1'b0;
-    end
+  //   SPI stop sending
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      spi_stopped_sending <= 1'b0;
+  end
 
-    //   SPI stop sending
-    always_ff @(posedge clk_i) begin
-      if (~rstn_i)
-        spi_stopped_sending <= 1'b0;
-    end
+  //   SPI Slave select
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      spi_ss_o <= 1'b1;
+    else if (spi_started_sending)
+      spi_ss_o <= 1'b0;
+    else if (spi_stopped_sending)
+      spi_ss_o <= 1'b1;
+  end
 
-    //   SPI Slave select
-    always_ff @(posedge clk_i) begin
-      if (~rstn_i)
-        spi_ss_o <= 1'b1;
-      else if (spi_started_sending)
-        spi_ss_o <= 1'b0;
-      else if (spi_stopped_sending)
-        spi_ss_o <= 1'b1;
-    end
+  //   SPI sclk
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      spi_sclk_o <= 1'b0;
+    else if (spi_sclk_counter == SCLK_COUNTER_MAX)
+      spi_sclk_o <= 1'b1;
+    else
+      spi_sclk_o <= 1'b0;
+  end
 
-    //   SPI sclk
-    always_ff @(posedge clk_i) begin
-      if (~rstn_i)
-        spi_sclk_o <= 1'b0;
-      else if (spi_sclk_counter == SCLK_COUNTER_MAX)
-        spi_sclk_o <= 1'b1;
-      else
-        spi_sclk_o <= 1'b0;
-    end
+  //   SPI sclk counter
+  always_ff @(posedge clk_i) begin
+    if (~rstn_i)
+      spi_sclk_counter <= 0;
+    else if(spi_sclk_counter < SCLK_COUNTER_MAX && spi_started_sending)
+      spi_sclk_counter++;
+    else
+      spi_sclk_counter <= 0;
+  end
 
-    //   SPI sclk counter
-    always_ff @(posedge clk_i) begin
-      if (~rstn_i)
-        spi_sclk_counter <= 0;
-      else if(spi_sclk_counter < SCLK_COUNTER_MAX && spi_started_sending)
-        spi_sclk_counter++;
-      else
-        spi_sclk_counter <= 0;
-    end
-
-    assign spi_done_o = ctrl_complete_bit;
+  assign spi_done_o = ctrl_complete_bit;
 
 endmodule
