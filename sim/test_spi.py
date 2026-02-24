d0218f2f67469999af2ee9b12d8d0040639f2bd0
carotium <coolboy9987@gmail.com>
2026-02-24 11:45:37 +0000

    Added spi transfer driver.

--- sim/test_spi.py
+++ sim/test_spi.py
@@ -3,23 +3,23 @@ from forastero.driver import DriverEvent
 from forastero import BaseBench
 
 from base import get_test_runner, WAVES
-from handshake.io import ObiChAIO, ObiChRIO
-from handshake.requestor import ObiChARequestDriver, ObiChRRequestMonitor
-from handshake.sequences import obi_channel_a_trans
-from handshake.transaction import ObiChATrans, ObiChRTrans
+from handshake.io import ObiChAIO, ObiChRIO, SpiIO
+from handshake.requestor import ObiChARequestDriver, ObiChRRequestMonitor, SpiRequestDriver
+from handshake.sequences import obi_channel_a_trans, spi_trans
+from handshake.transaction import ObiChATrans, ObiChRTrans, SpiTransferTrans
 
 class SpiImpTB(BaseBench):
     def __init__(self, dut):
         super().__init__(dut, clk=dut.clk_i, rst=dut.rstn_i, rst_active_high=False)
         obi_a_io = ObiChAIO(dut, "obi", IORole.RESPONDER, io_style=io_suffix_style)
         obi_r_io = ObiChRIO(dut, "obi", IORole.RESPONDER, io_style=io_suffix_style)
+        spi_io = SpiIO(dut, "spi", IORole.RESPONDER, io_style=io_suffix_style)
 
         self.register("obi_a_drv", ObiChARequestDriver(self, obi_a_io, self.clk, self.rst))
-        #self.register("obi_a_monitor", ObiChARequestMonitor(self, obi_a_io, self.clk, self.rst))
 
         self.register("obi_r_monitor", ObiChRRequestMonitor(self, obi_r_io, self.clk, self.rst))
-        # Register callback on input driver to push reference to monitor
-        #self.input_driver.subscribe(DriverEvent.ENQUEUE, self.push_reference)
+
+        self.register("spi_drv", SpiRequestDriver(self, spi_io, self.clk, self.rst))
 
 
 @SpiImpTB.testcase(
@@ -36,18 +36,27 @@ async def random_traffic(tb: SpiImpTB, log):
     tb.scoreboard.channels["obi_r_monitor"].push_reference(ObiChRTrans(rdata=87))
 
     # Write to data reg and read from ctrl reg
-    tb.scoreboard.channels["obi_r_monitor"].push_reference(ObiChRTrans(rdata=1))
+    tb.scoreboard.channels["obi_r_monitor"].push_reference(ObiChRTrans(rdata=2))
+
+    #tb.scoreboard.channels["obi_r_monitor"].push_reference(ObiChRTrans(rdata=1))
 
 
     trans = [
         ObiChATrans(addr=0x0000, wdata=87, we=True, be=0x1),
         ObiChATrans(addr=0x0000, we=False, be=0x1),
 
-        ObiChATrans(addr=0x0000, wdata=0x1, we=True, be=0x1),
-        ObiChATrans(addr=0x0000, we=False, be=0x1),
+        ObiChATrans(addr=0x0001, wdata=0x1, we=True, be=0x1),
+        ObiChATrans(addr=0x0001, we=False, be=0x1),
+
+        #ObiChATrans(addr=0x0001, wdata=0, we=True, be=1),
+    ]
+
+    tran_spi = [
+        SpiTransferTrans(miso=True),
     ]
 
     tb.schedule(obi_channel_a_trans(obi_a_drv=tb.obi_a_drv, trans=trans))
+    #tb.schedule(spi_trans(spi_drv=tb.spi_drv, trans=tran_spi))
 
 def test_spi_runner():
     runner = get_test_runner("spi_imp")
