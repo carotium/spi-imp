f91e4bd813a45b6c8eb96e2e82f50f450c81bef9
carotium <coolboy9987@gmail.com>
2026-03-03 07:26:39 +0000

    Removed unused responder script.

--- sim/obi/responder.py
+++
@@ -1,5 +0,0 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2023-2024 Vypercore. All Rights Reserved
-
-from cocotb.triggers import ClockCycles, RisingEdge
-from forastero.driver import BaseDriver
